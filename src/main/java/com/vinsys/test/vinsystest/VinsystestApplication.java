4a7a2799e28eabd3ab8378a7571056e203fafd7c
root <root@localhost.localdomain>
2018-09-22 07:19:03 -0400

    VIN-1

--- src/main/java/com/vinsys/test/vinsystest/VinsystestApplication.java
+++ src/main/java/com/vinsys/test/vinsystest/VinsystestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VinsystestApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(VinsystestApplication.class, args);
 	}
 }
