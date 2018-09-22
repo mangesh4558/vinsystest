41edf9ee388ef71859271007c52f2577e8d14f5a
root <root@localhost.localdomain>
2018-09-22 07:13:59 -0400

    first commit

---
+++ src/main/java/com/vinsys/test/vinsystest/VinsystestApplication.java
@@ -0,0 +1,12 @@
+package com.vinsys.test.vinsystest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinsystestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinsystestApplication.class, args);
+	}
+}
